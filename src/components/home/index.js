1df17ee1f219e223c95b876a65d8b060d6556bbe
Nikita Mundra <nikitam.chapter247@gmail.com>
2019-05-21 12:21:16 +0530

    New changes in signup

---
+++ src/components/home/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {Header} from './components/Header';
+import {Main} from './components/Main';
+import {Footer} from './components/Footer';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="App Site">
+                <div className="Site-content">
+                    <div className="App-header">
+                        <Header />
+                    </div>
+                    <div className="main">
+                        <Main />
+                    </div>
+                </div>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default Home;
